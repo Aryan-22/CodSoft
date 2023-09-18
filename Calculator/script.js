e6556d63128035ab6b005fa37a0547d6f752ab91
Ariyan22 <80637549+Aryan-22@users.noreply.github.com>
2023-09-18 23:18:19 +0530

    Update script.js

--- Calculator/script.js
+++ Calculator/script.js
@@ -5,7 +5,7 @@ var icon=document.getElementById('icon');
 icon.onclick=function(){
     document.body.classList.toggle("dark-theme");
     if(document.body.classList.contains("dark-theme")){
-        icon.src="./dark theme icon/sun.png";
+        icon.src="../dark theme icon/sun.png";
     }
 }
 
